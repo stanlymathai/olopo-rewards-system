d7e94c5748ee681f157f2f2159c248e897810530
stanlymathai <stanly@math.ai>
2024-08-27 10:55:59 +0530

    chore; add index route with child routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
+const route = require('./points.route');
 
-router.get('/health', function (_, res) {
+function healthRoute(_, res) {
   return res.status(200).send('As Strong an an Ox!');
-});
+}
 
-router.use('/users', require('./user.route'));
+router.get('/health', healthRoute);
+
+router.use('/points', route);
 
 module.exports = router;
