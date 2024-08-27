7b982039d83a5d6f9b9c5675a0570b9b939713ff
stanlymathai <stanly@math.ai>
2024-08-27 14:00:27 +0530

    chore; add accrual route.

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const route = require('./points.route');
+
+const pointsRoute = require('./points.route');
+const accrualRoute = require('./accrual.route');
 
 function healthRoute(_, res) {
   return res.status(200).send('As Strong an an Ox!');
@@ -7,6 +9,7 @@ function healthRoute(_, res) {
 
 router.get('/health', healthRoute);
 
-router.use('/points', route);
+router.use('/points', pointsRoute);
+router.use('/accrual', accrualRoute);
 
 module.exports = router;
